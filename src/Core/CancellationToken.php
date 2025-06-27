cf28d930c191a372c3f1599f54609f35f1085a75
majidmohammadian <majeedmohammadian@gmail.com>
2025-06-27 18:46:01 +0330

    added CancellationToken class

---
+++ src/Core/CancellationToken.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace JobMetric\Flyron\Core;
+
+/**
+ * Class CancellationToken
+ *
+ * Represents a token that signals whether an operation has been cancelled.
+ * Useful for cooperative cancellation in asynchronous or long-running processes.
+ *
+ * This token can be passed to asynchronous tasks or fibers to allow them to
+ * check whether they should stop execution early.
+ *
+ * @package JobMetric\Flyron
+ */
+class CancellationToken
+{
+    /**
+     * Indicates whether cancellation has been requested.
+     *
+     * @var bool
+     */
+    private bool $cancelled = false;
+
+    /**
+     * Request cancellation.
+     *
+     * Marks the token as cancelled, signaling any observers or tasks
+     * that they should attempt to stop execution as soon as possible.
+     *
+     * @return void
+     */
+    public function cancel(): void
+    {
+        $this->cancelled = true;
+    }
+
+    /**
+     * Check if cancellation has been requested.
+     *
+     * @return bool Returns true if cancellation has been requested; otherwise false.
+     */
+    public function isCancelled(): bool
+    {
+        return $this->cancelled;
+    }
+}
