5788fd77081154152176bc61ecf5b82e89149772
majidmohammadian <majeedmohammadian@gmail.com>
2025-07-02 23:50:37 +0330

    added async helper method

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,22 @@
 <?php
+
+use JobMetric\Flyron\Concurrency\CancellationToken;
+use JobMetric\Flyron\Concurrency\Promise;
+use JobMetric\Flyron\Facades\Async;
+
+if (! function_exists('async')) {
+    /**
+     * Run an async task with Flyron easily.
+     *
+     * @param callable $callback
+     * @param array $args
+     * @param int|null $timeout
+     * @param CancellationToken|null $token
+     *
+     * @return Promise
+     */
+    function async(callable $callback, array $args = [], ?int $timeout = null, ?CancellationToken $token = null): Promise
+    {
+        return Async::run($callback, $args, $timeout, $token);
+    }
+}
