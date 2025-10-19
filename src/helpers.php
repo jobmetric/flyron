5de284b93c8f64b056ad77f9edfdf08a1b43a1f3
majidmohammadian <majeedmohammadian@gmail.com>
2025-10-20 01:18:43 +0330

    added async_process method in helper file

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,7 @@
 use JobMetric\Flyron\Concurrency\CancellationToken;
 use JobMetric\Flyron\Concurrency\Promise;
 use JobMetric\Flyron\Facades\Async;
+use JobMetric\Flyron\Facades\AsyncProcess;
 
 if (! function_exists('async')) {
     /**
@@ -18,5 +19,24 @@ if (! function_exists('async')) {
     function async(callable $callback, array $args = [], ?int $timeout = null, ?CancellationToken $token = null): Promise
     {
         return Async::run($callback, $args, $timeout, $token);
+    }
+}
+
+if (! function_exists('async_process')) {
+    /**
+     * Run a background process using Flyron AsyncProcess with a simple helper.
+     *
+     * The callable will be serialized and executed via `flyron:exec` in a separate
+     * PHP process. See config('flyron.process') for security and throttling options.
+     *
+     * @param callable $callback The closure to run in background.
+     * @param array    $args     Arguments passed to the closure.
+     * @param array    $options  example: {cwd?: string|null, env?: array|null, timeout?: float|null, idle_timeout?: float|null, disable_output?: bool|null, label?: string|null}
+     *
+     * @return int|null The spawned process PID or null if unavailable.
+     */
+    function async_process(callable $callback, array $args = [], array $options = []): ?int
+    {
+        return AsyncProcess::run($callback, $args, $options);
     }
 }
